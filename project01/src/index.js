725291653fe174fdadc6a001f115e535a0693a0d
apple <251556682@qq.com>
2019-06-09 19:50:46 +0800

    Webpack打包一个简单的项目示例

---
+++ project01/src/index.js
@@ -0,0 +1,2 @@
+document.write('初始化项目');
+document.writeln('dddd')
---
+++ project01/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+    entry:'./src/index.js',
+    mode:'production',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+}
